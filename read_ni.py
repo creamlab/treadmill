b43f688d058293917ef8474264b9432077f8a601
QuentinDecultot <q.decultot@gmail.com>
2021-04-09 14:48:15 +0200

    boutons_start_stop

--- read_ni.py
+++ read_ni.py
@@ -1,74 +1,223 @@
-Microsoft Windows [version 10.0.19041.867]
-(c) 2020 Microsoft Corporation. Tous droits r‚serv‚s.
+"""
+Analog data acquisition for QuSpin's OPMs via National Instruments' cDAQ unit
+The following assumes:
+"""
 
-C:\Users\quent\Documents\Stage_tapis> cd C:\Users\quent
+# Imports
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import tkinter as tk
+from datetime import datetime
 
-C:\Users\quent> pip install tkinter
-ERROR: Could not find a version that satisfies the requirement tkinter
-ERROR: No matching distribution found for tkinter
+from scipy.interpolate import interp1d
+from scipy.interpolate import splev, splrep
 
-C:\Users\quent> git
-usage: git [--version] [--help] [-C <path>] [-c <name>=<value>]
-           [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
-           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
-           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
-           [--super-prefix=<path>] [--config-env=<name>=<envvar>]
-           <command> [<args>]
+import nidaqmx
+from nidaqmx.stream_readers import AnalogMultiChannelReader
+from nidaqmx import constants
+# from nidaqmx import stream_readers  # not needed in this script
+# from nidaqmx import stream_writers  # not needed in this script
 
-These are common Git commands used in various situations:
+import threading
+import pickle
+from datetime import datetime
+import scipy.io
 
-start a working area (see also: git help tutorial)
-   clone             Clone a repository into a new directory
-   init              Create an empty Git repository or reinitialize an existing one
 
-work on the current change (see also: git help everyday)
-   add               Add file contents to the index
-   mv                Move or rename a file, a directory, or a symlink
-   restore           Restore working tree files
-   rm                Remove files from the working tree and from the index
-   sparse-checkout   Initialize and modify the sparse-checkout
+# Parameters
+sampling_freq_in = 1000  # in Hz
+buffer_in_size = 100
+bufsize_callback = buffer_in_size
+buffer_in_size_cfg = round(buffer_in_size * 1)  # clock configuration
+chans_in = 6  # set to number of active OPMs (x2 if By and Bz are used, but that is not recommended)
+refresh_rate_plot = 10  # in Hz
+crop = 10  # number of seconds to drop at acquisition start before saving
+my_filename = 'test_3_opms'  # with full path if target folder different from current folder (do not leave trailing /)
 
-examine the history and state (see also: git help revisions)
-   bisect            Use binary search to find the commit that introduced a bug
-   diff              Show changes between commits, commit and working tree, etc
-   grep              Print lines matching a pattern
-   log               Show commit logs
-   show              Show various types of objects
-   status            Show the working tree status
 
-grow, mark and tweak your common history
-   branch            List, create, or delete branches
-   commit            Record changes to the repository
-   merge             Join two or more development histories together
-   rebase            Reapply commits on top of another base tip
-   reset             Reset current HEAD to the specified state
-   switch            Switch branches
-   tag               Create, list, delete or verify a tag object signed with GPG
 
-collaborate (see also: git help workflows)
-   fetch             Download objects and refs from another repository
-   pull              Fetch from and integrate with another repository or a local branch
-   push              Update remote refs along with associated objects
+# Initialize data placeholders
+buffer_in = np.zeros((chans_in, buffer_in_size))
+data = np.zeros((chans_in, 1))  # will contain a first column with zeros but that's fine
 
-'git help -a' and 'git help -g' list available subcommands and some
-concept guides. See 'git help <command>' or 'git help <concept>'
-to read about a specific subcommand or concept.
-See 'git help git' for an overview of the system.
 
-C:\Users\quent> cd C:\Users\quent\Documents\Stage_tapis
+# Definitions of basic functions
+def ask_user():
+    global running
+    input("Acquisition starting")
+    running = False
 
-C:\Users\quent\Documents\Stage_tapis> git clone https://github.com/creamlab/treadmill.git
-Cloning into 'treadmill'...
 
-C:\Users\quent\Documents\Stage_tapis> git status
-fatal: not a git repository (or any of the parent directories): .git
+def cfg_read_task(acquisition):  # uses above parameters
+    acquisition.ai_channels.add_ai_voltage_chan("Dev2/ai0:5")  # has to match with chans_in
+    acquisition.timing.cfg_samp_clk_timing(rate=sampling_freq_in, sample_mode=constants.AcquisitionType.CONTINUOUS,
+                                           samps_per_chan=buffer_in_size_cfg)
 
-C:\Users\quent\Documents\Stage_tapis> cd treadmill
 
-C:\Users\quent\Documents\Stage_tapis\treadmill> git status
-On branch main
-Your branch is up to date with 'origin/main'.
+def reading_task_callback(task_idx, event_type, num_samples, callback_data):  # bufsize_callback is passed to num_samples
+    global data
+    global buffer_in
+    global time_start
 
-nothing to commit, working tree clean
+    if running:
+        # It may be wiser to read slightly more than num_samples here, to make sure one does not miss any sample,
+        # see: https://documentation.help/NI-DAQmx-Key-Concepts/contCAcqGen.html
+        buffer_in = np.zeros((chans_in, num_samples))  # double definition ???
+        stream_in.read_many_sample(buffer_in, num_samples, timeout=constants.WAIT_INFINITELY)
+        with open("treadmill_"+str(entry.get())+'_'+str(date)+".txt","a") as file:
+            current_time=time.time()
+            for i in range(len(buffer_in[0])):
+                file.write(str(current_time-time_start+i)+','+str(buffer_in[0][i])+','+str(buffer_in[1][i])+','+str(buffer_in[2][i])+','+str(buffer_in[3][i])+','+str(buffer_in[4][i])+','+str(buffer_in[5][i])+'\n')
+        data = np.append(data, buffer_in, axis=1)  # appends buffered data to total variable data
 
-C:\Users\quent\Documents\Stage_tapis\treadmill>
+    return 0  # Absolutely needed for this callback to be well defined (see nidaqmx doc).
+
+date=str(datetime.now())
+date=date.replace(':','-')
+date=date.replace(' ','_')
+date=date[:-7]
+window = tk.Tk()
+label = tk.Label(text="Data collecting tool",width=30,height=10)
+label.pack()
+victim_name = tk.Label(text="Victim's name")
+entry = tk.Entry()
+victim_name.pack()
+entry.pack()
+button_Start = tk.Button(text="Start",width=25,height=5,)
+button_Start.pack()
+button_Stop = tk.Button(text="Stop",width=25,height=5,)
+button_Stop.pack()
+information = tk.Label()
+information.pack()
+
+
+def start_acquisition():
+    global button_Start
+    global running  #cette variable ne sert plus à rien
+    global stream_in
+    global task_in
+    global information
+    global time_start
+
+    time_start=time.time()
+    # Configure and setup the tasks
+    task_in = nidaqmx.Task()
+    cfg_read_task(task_in)
+    stream_in = AnalogMultiChannelReader(task_in.in_stream)
+    task_in.register_every_n_samples_acquired_into_buffer_event(bufsize_callback, reading_task_callback)
+
+
+    # Start threading to prompt user to stop
+    thread_user = threading.Thread(target=ask_user)
+    thread_user.start()
+
+
+    # Main loop
+    running = True
+    # time_start = datetime.now()
+    task_in.start()
+
+def stop_acquisition():
+    global button_Stop
+    global running
+    running = False
+    task_in.close()
+    print('\n'+"Acquisition stopped")
+
+# def start_time():
+#     global button_Start
+#     global time_start
+#     time_start=time.time()
+
+# button_Start.config(command = start_time)
+button_Start.config(command = start_acquisition)
+button_Stop.config(command = stop_acquisition)
+window.mainloop()
+
+
+
+
+# Plot a visual feedback for the user's mental health
+# f, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(6, 1, sharex='all', sharey='none')    
+
+
+# while running:  # make this adapt to number of channels automatically
+#     print(data)
+#     current_time=time.time()
+#     if current_time-time_start>1:
+#         running=False
+#         print(current_time-time_start)
+    # ax1.clear()
+    # ax2.clear()
+    # ax3.clear()
+    # ax4.clear()
+    # ax5.clear()
+    # ax6.clear()
+    # ax1.plot(data[0, -sampling_freq_in * 5:].T)  # 5 seconds rolling window
+    # ax2.plot(data[1, -sampling_freq_in * 5:].T)
+    # ax3.plot(data[2, -sampling_freq_in * 5:].T)
+    # ax4.plot(data[3, -sampling_freq_in * 5:].T)  # 5 seconds rolling window
+    # ax5.plot(data[4, -sampling_freq_in * 5:].T)
+    # ax6.plot(data[5, -sampling_freq_in * 5:].T)
+    # # Label and axis formatting
+    # ax6.set_xlabel('time [s]')
+    # ax1.set_ylabel('Force_axe_X_gauche')
+    # ax2.set_ylabel('Force_axe_Y_gauche')
+    # ax3.set_ylabel('Force_axe_Z_gauche')
+    # xticks = np.arange(0, data[0, -sampling_freq_in * 5:].size, sampling_freq_in)
+    # xticklabels = np.arange(0, xticks.size, 1)
+    # ax3.set_xticks(xticks)
+    # ax3.set_xticklabels(xticklabels)
+
+    # plt.pause(1/refresh_rate_plot)  # required for dynamic plot to work (if too low, nulling performance bad)
+
+# Close task to clear connection once done
+# task_in.close()
+# duration = datetime.now() - time_start
+
+
+
+# # Final save data and metadata ... first in python reloadable format:
+# filename = my_filename
+# with open(filename, 'wb') as f:
+#     pickle.dump(data, f)
+# '''
+# Load this variable back with:
+# with open(name, 'rb') as f:
+#     data_reloaded = pickle.load(f)
+# '''
+# # Human-readable text file:
+# extension = '.txt'
+# np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # turn off summarization, line-wrapping
+# with open(filename + extension, 'w') as f:
+#     f.write(np.array2string(data.T, separator=', '))  # improve precision here!
+# # Now in matlab:
+# extension = '.mat'
+# scipy.io.savemat(filename + extension, {'data':data})
+
+
+# # Some messages at the end
+# num_samples_acquired = data[0,:].size
+# print("\n")
+# print("OPM acquisition ended.\n")
+# print("Acquisition duration: {}.".format(duration))
+# print("Acquired samples: {}.".format(num_samples_acquired - 1))
+
+
+# # Final plot of whole time course the acquisition
+# plt.close('all')
+# f_tot, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='all', sharey='none')
+# ax1.plot(data[0, 10:].T)  # note the exclusion of the first 10 iterations (automatically zoomed in plot)
+# ax2.plot(data[1, 10:].T)
+# ax3.plot(data[2, 10:].T)
+# # Label formatting ...
+# ax3.set_xlabel('time [s]')
+# ax1.set_ylabel('voltage [V]')
+# ax2.set_ylabel('voltage [V]')
+# ax3.set_ylabel('voltage [V]')
+# xticks = np.arange(0, data[0, :].size, sampling_freq_in)
+# xticklabels = np.arange(0, xticks.size, 1)
+# ax3.set_xticks(xticks)
+# ax3.set_xticklabels(xticklabels)
+# plt.show()
