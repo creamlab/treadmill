7cfe06bc61db1f81c29995121f4c4636161b48d5
JJ Aucouturier <aucouturier@gmail.com>
2021-04-13 12:24:58 +0200

    fixed start time; reformat class variables

--- Launcher.py
+++ Launcher.py
@@ -1,39 +1,37 @@
-import module_acquisition as mo
+import ni_reader as ni
 from datetime import datetime
 import time
 import tkinter as tk
 
+# current date
+date=datetime.now().strftime('%y_%m_%d_%H_%M')
 
-
-date=str(datetime.now())
-date=date.replace(':','-')
-date=date.replace(' ','_')
-date=date[:-7]
+# create GUI
 window = tk.Tk()
 label = tk.Label(text="Data collecting tool",width=30,height=10)
 label.pack()
-individu = tk.Label(text="individu")
-entry = tk.Entry()
-individu.pack()
-entry.pack()
+participant = tk.Label(text="Participant")
+participant.pack()
+text_field = tk.Entry()
+text_field.pack()
 button_Start = tk.Button(text="Start",width=25,height=5,)
 button_Start.pack()
 button_Stop = tk.Button(text="Stop",width=25,height=5,)
 button_Stop.pack()
-information = tk.Label()
-information.pack()
-data_samples=mo.data_acquisitor(entry,date)
 
+# NI GUI methods
+ni_reader=ni.NIReader(date)
 def start():
-	global button_Start
-	data_samples.start_acquisition(entry.get())
+	ni_reader.set_participant(text_field.get())
+	ni_reader.start_acquisition()
 
 def stop():
-	global button_Stop
-	data_samples.stop_acquisition()
-
+	ni_reader.stop_acquisition()
 button_Start.config(command = start)
 button_Stop.config(command = stop)
+
+
+# start GUI
 window.mainloop()
 
 
